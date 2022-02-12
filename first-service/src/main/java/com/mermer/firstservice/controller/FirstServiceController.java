a79d23e8957a01d56c677b4eafacde0eb8f464f5
mermer <mincoln419@naver.com>
2022-02-12 18:06:36 +0900

    #15 gate-way filter 적용
    #15 gate-way filter 적용
    header request, response 확인 테스트

--- first-service/src/main/java/com/mermer/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/mermer/firstservice/controller/FirstServiceController.java
@@ -1,11 +1,15 @@
 package com.mermer.firstservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
 	@GetMapping("/welcome")
@@ -13,4 +17,9 @@ public class FirstServiceController {
 		return "Welcome to First-Service";
 	}
 	
+	@GetMapping("/message")
+	public String message(@RequestHeader("first-request") String header) {
+		log.info(header);
+		return "Message of First Service";
+	}
 }
--- second-service/src/main/java/com/mermer/secondservice/controller/SecondServiceController.java
+++ second-service/src/main/java/com/mermer/secondservice/controller/SecondServiceController.java
@@ -1,11 +1,15 @@
 package com.mermer.secondservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
 
 	@GetMapping("/welcome")
@@ -13,4 +17,10 @@ public class SecondServiceController {
 		return "Welcome to Second-Service";
 	}
 	
+	
+	@GetMapping("/message")
+	public String message(@RequestHeader("second-request") String header) {
+		log.info(header);
+		return "Message of Second Service";
+	}
 }
