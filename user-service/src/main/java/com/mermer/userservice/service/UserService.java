a5fa7c6a1ee94164ef6381b4c48d679b2762f26f
mermer <mincoln419@naver.com>
2022-03-11 09:43:46 +0900

    #23 user-service get Users add
    #23 user-service get Users add

--- user-service/src/main/java/com/mermer/userservice/service/UserService.java
+++ user-service/src/main/java/com/mermer/userservice/service/UserService.java
@@ -1,11 +1,19 @@
 package com.mermer.userservice.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import com.mermer.userservice.dto.UserDto;
+import com.mermer.userservice.entity.UserEntity;
+import com.mermer.userservice.vo.ResponseUser;
 
 public interface UserService {
 
 	ResponseEntity createUser(UserDto userDto);
+
+	Iterable<UserEntity> getUserByAll();
+
+	
 	
 }
