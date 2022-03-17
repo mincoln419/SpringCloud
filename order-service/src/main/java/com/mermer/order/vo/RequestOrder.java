e16dca7936be9c2f436a8d1cb4a5c50e6104e7f4
mermer <mincoln419@naver.com>
2022-03-18 08:56:41 +0900

    #24 order service controller 작업
    #24 order service controller 작업

---
+++ order-service/src/main/java/com/mermer/order/vo/RequestOrder.java
@@ -0,0 +1,15 @@
+package com.mermer.order.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RequestOrder {
+
+	private String productId;
+	private Integer qty;
+	private Integer unitPrice;
+	
+}
