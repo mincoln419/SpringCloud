5157da16fd0d901452eab0c07dae62e390907428
mermer <mincoln419@naver.com>
2022-03-19 13:57:55 +0900

    #23 user-service security 설정
    #23 user-service security 설정

--- user-service/src/main/java/com/mermer/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/mermer/userservice/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mermer.userservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -8,6 +9,9 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -48,6 +52,16 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public Iterable<UserEntity> getUserByAll() {
 		return userRepository.findAll();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		UserEntity userEntity = userRepository.findByEmail(username);
+		
+		if(userEntity == null)throw new UsernameNotFoundException(username);
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(), true, true, true, true, new ArrayList<>());
 	}
 
 	
