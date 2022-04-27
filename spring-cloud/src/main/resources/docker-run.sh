ff955ee4384c6328fb65cb62e9997e7d266ff494
mingeon Cho <mincoln419@naver.com>
2022-04-28 08:47:06 +0900

    refs #62 spring config 배포

--- spring-cloud/src/main/resources/docker-run.sh
+++ spring-cloud/src/main/resources/docker-run.sh
@@ -14,3 +14,20 @@ docker pull edowon0623/user-service:1.0
 
 mvn clean complie package -DskipTests=true
 
+
+docker networkd create --driver bridge
+
+docker system prune
+
+docker network ls
+
+docker network create --gateway 172.18.0.1 --subnet 172.18.0.0/16 ecommerce-network
+
+docker network inspect ecommerce-network
+
+
+
+docker run -d --name rabbitmq --network ecommerce-network \
+ -p 15672:15672 -p 5672:5672 -p 15671:15671 -p 5671:5671 -p 4369:4369 \
+ -e RABBITMQ_DEFAULT_USER=guest \
+ -e RABBITMQ_DEFAULT_PASS=guest rabbitmq:management
